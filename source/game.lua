fa65cbae9f7fa9e12ec9fbc38b2c660b3391f439
Carmon <elcarmon@gmail.com>
2024-08-01 22:34:32 -0300

    Add camera movement.

--- source/game.lua
+++ source/game.lua
@@ -5,38 +5,56 @@ local gfx <const> = playdate.graphics
 local geo <const> = playdate.geometry
 local displayWidth <const>, displayHeight <const> = playdate.display.getSize()
 local halfDisplayWidth <const> = displayWidth/2
+local halfDisplayHeight <const> = displayHeight/2
+
+local CAM_SPEED = 5
 
 function Game:new()
   local self = {}
   
   local camPos
+  local camDir = geo.point.new(0, 0)
+
   local isDead
-
-  local press = function()
-    ball:setSlide(true)
-  end
-
-  local up = function()
-    ball:setSlide(false)
-  end
-
+  
   local myInputHandlers = {
-    AButtonDown = press,
-    BButtonDown = press,
-    AButtonUp = up,
-    BButtonUp = up,
+    upButtonDown = function ()
+      camDir.y -= 1
+    end,
+    upButtonUp = function ()
+      camDir.y += 1
+    end,
+    downButtonDown = function ()
+      camDir.y += 1
+    end,
+    downButtonUp = function ()
+      camDir.y -= 1
+    end,
+    leftButtonDown = function ()
+      camDir.x -= 1
+    end,
+    leftButtonUp = function ()
+      camDir.x += 1
+    end,
+    rightButtonDown = function ()
+      camDir.x += 1
+    end,
+    rightButtonUp = function ()
+      camDir.x -= 1
+    end
   }
 
   local dirty
   function self:start()
     playdate.inputHandlers.push(myInputHandlers) -- only pushed? somehow menu push overrides it
     -- Only way to draw segments and sprites
-    gfx.sprite.setBackgroundDrawingCallback(
-      function(x, y, width, height)
-        if not dirty then return end
-        dirty = false
-      end
-    )
+    -- gfx.sprite.setBackgroundDrawingCallback(
+    --   function(x, y, width, height)
+    --     if not dirty then return end
+    --     gfx.drawCircleAtPoint(halfDisplayWidth, halfDisplayHeight, 400)
+    --     dirty = false
+    --   end
+    -- )
 
     self:reset()
   end
@@ -54,25 +72,19 @@ function Game:new()
   function self:update()    
     if isDead then return end -- don't update if dead
     if playdate.isCrankDocked() then pauseGame() end -- this fn lives on main
+
+    print(camPos)
+    setColor('dark')
+    gfx.fillCircleAtPoint(20, 20, 50)
     
     -- camera
-    -- local newX = math.floor(newPos.x - halfDisplayWidth)
-    -- local motion = false
-    -- if newX ~= -camPos.x then
-    --   camPos.x = -newX
-    --   motion = true
-    -- end
-    -- local newY = newPos.y - (displayHeight-100)
-    -- if newY ~= -camPos.y then
-    --   camPos.y = -newY
-    --   motion = true
-    -- end
-    
-    -- if motion then
-    --   gfx.setDrawOffset(camPos.x, camPos.y)
-    --   gfx.sprite.redrawBackground()
-    --   dirty = true
-    -- end
+    if camDir.x ~= 0 or camDir.y ~= 0 then
+      camPos.x += camDir.x * CAM_SPEED
+      camPos.y += camDir.y * CAM_SPEED
+      gfx.setDrawOffset(camPos.x, camPos.y)
+      -- gfx.sprite.redrawBackground()
+      dirty = true
+    end
   end
 
   return self
